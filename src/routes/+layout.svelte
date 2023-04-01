ed154600cd6042fb876a718a0d2172b0c8fbfdab
Person <localboxcrox@gmail.com>
2023-04-02 05:16:02 +0530

    test2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,57 @@
-<script>
+<script lang="ts">
 	import './styles.css';
 </script>
 
-<slot />
+
+
+<div class="ani_cont"><div class="cr1"></div></div>
+
+
+<slot/>
+
+
+
+<style>
+    .cr1 {
+        animation-name: mv1;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        /* animation-direction: alternate; */
+        background-color: blueviolet;
+
+        border-radius: 50%;
+    }
+
+    @keyframes mv1 {
+        0% {
+            border-top-right-radius: 20%;
+            border-top-left-radius: 90%;
+            border-bottom-left-radius: 20%;
+            border-bottom-right-radius: 70%;
+
+            height: 30vh;
+            width: 30vh;
+
+        }
+        33% {
+            border-top-right-radius: 70%;
+            border-top-left-radius: 60%;
+            border-bottom-left-radius: 90%;
+            border-bottom-right-radius: 60%;
+
+            height: 21vh;
+            width: 21vh;
+
+        }
+        100% {
+            border-top-right-radius: 20%;   
+            border-top-left-radius: 90%;
+            border-bottom-left-radius: 20%;
+            border-bottom-right-radius: 70%;
+
+            height: 30vh;
+            width: 30vh;
+
+        }
+    }
+</style>
