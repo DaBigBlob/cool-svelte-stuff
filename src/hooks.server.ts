b0b1a53a3ef79d132b3e183950dc76d063343cdf
Person <localboxcrox@gmail.com>
2023-04-02 17:32:29 +0530

    test4

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,8 @@ import type { Handle } from '@sveltejs/kit';
  
 export const handle = (async ({ event, resolve }) => {
  
-  if (!building && !event.request.headers.get("Accept")?.includes("text/html")) {
+  //!building is a gross hack; idially adapter-cloudflare should have Accept: text/html
+  if (!event.request.headers.get("Accept")?.includes("text/html") && !building) {
     return new Response("Please visit from a browser.", {headers: {"Content-Type": "text/html"}, status: 406});
   }
 
