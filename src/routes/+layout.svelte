abd86c86652c7c7bef58659f8d6968b61dd05ff6
Person <localboxcrox@gmail.com>
2023-04-02 22:35:26 +0530

    test4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,40 +19,50 @@
 
 
 <style>
+    :root {
+        height: 100vh;
+        width: 100vw;
+    }
+    * {
+        margin: 0%;
+        border: 0%;
+        padding: 0%;
+    }
+
     .ani_cont {
         position: absolute;
         height: 100vh;
         width: 100vw;
         z-index: -1;
+        overflow-y: hidden;
     }
 
     .ani_cont > div {
         --perfect-rad: min(98vh, 98vw);
-        --left-center: calc(50vw - var(--perfect-rad)/2);
         height: var(--perfect-rad);
         width: var(--perfect-rad);
 
         z-index: inherit;
         position: inherit;
-        left: var(--left-center);
+        left: calc(50vw - var(--perfect-rad)/2);
+        top: calc(50vh - var(--perfect-rad)/2);
 
         border-radius: 50%;
-        
     }
 
     .ani_cont > div > div {
-        height: 35%;
-        width: 35%;
+        height: 25%; 
+        width: 25%;
 
         z-index: inherit;
         position: relative;
-        left: 16%;
-        top: 16%;
+        left: 17%;
+        top: 17%;
 
-        border-top-right-radius: 98%;
+        border-top-right-radius: 90%;
         border-top-left-radius: 0%;
-        border-bottom-left-radius: 98%;
-        border-bottom-right-radius: 0%;
+        border-bottom-left-radius: 90%;
+        border-bottom-right-radius: 20%;
 
         background-color: rgba(52, 110, 95, 0.344);
     }
